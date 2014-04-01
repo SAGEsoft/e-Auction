bf9cdc919eb2274b1854a54c4c8b7c7cc687164a
mbe5038 <espejo.marius@gmail.com>
2014-04-01 16:46:14 -0400

    moved a line in item list and added auction attributes to item model

--- app/models/ItemsModel.js
+++ app/models/ItemsModel.js
@@ -6,13 +6,18 @@ module.exports = function(sequelize, DataTypes) {
 			title: DataTypes.STRING,
 			description: DataTypes.TEXT,
 			url: DataTypes.STRING,
-			location: DataTypes.INTEGER,
+			//location: DataTypes.INTEGER, /* address of seller */
 			category_id: DataTypes.STRING,
-			auto_id: DataTypes.INTEGER
+			auto_id: DataTypes.INTEGER,
+			buyer_id: DataTypes.INTEGER, /* id of most recent bidder */
+			current_bid: DataTypes.FLOAT,
+			buy_it_now: DataTypes.FLOAT,
+			reserve_price: DataTypes.FLOAT
 		},
 		{
 			associate: function(models){
 				Item.belongsTo(models.User);
+				Item.belongsTo(models.Address);
 			}
 		}
 	);
