ec0a1461056ddae8e77ae52416fddc8c07986fc1
Brandon Gift <bagift10@gmail.com>
2014-04-01 19:24:54 -0400

    Added directives for sidebar and searchbar
    Can now include sidebar with: <div sidebar></div>
    and searchbar with: <div searchbar></div>

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
-angular.module('eAuction', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'eAuction.system', 'eAuction.articles', 'eAuction.items', 'eAuction.addresses']);
+angular.module('eAuction', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'eAuction.system', 'eAuction.articles', 'eAuction.items', 'eAuction.addresses', 'eAuction.sidebar', 'eAuction.searchbar']);
 
 angular.module('eAuction.system', []);
 angular.module('eAuction.articles', []);
 angular.module('eAuction.items', []);
 angular.module('eAuction.addresses', []);
+angular.module('eAuction.sidebar', []);
+angular.module('eAuction.searchbar', []);
