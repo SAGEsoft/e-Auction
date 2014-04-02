8f7597412dbf30a7ce244ce90ea8ca9af2577da8
mbe5038 <espejo.marius@gmail.com>
2014-04-01 20:20:12 -0400

    made signin/signup pages look nicer

--- app/models/CreditCardModel.js
+++ app/models/CreditCardModel.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
 	var Credit_Card = sequelize.define('Credit_Card', {
 			card_num: DataTypes.INTEGER(16),
 			card_type: DataTypes.STRING(10),
-			exp: DataTypes.DATE
+			expiration: DataTypes.DATE
 		},
 		{
 			associate: function(models){
